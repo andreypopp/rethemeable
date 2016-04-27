f475bdec3926272d2a9d153a779867f4ac113cbf
Ryan Cline <rcline@shutterstock.com>
2016-04-26 19:33:59 -0600

    ApplyTheme was not exported.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
  * @copyright 2015, Andrey Popp <8mayday@gmail.com>
  */
 
+export ApplyTheme         from './ApplyTheme';
 export Themeable          from './Themeable';
-export Themed             from './Themed';
 export {default as theme} from './themeComponent';
