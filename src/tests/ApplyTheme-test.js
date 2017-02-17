15b0a5280bb5aaae6da19eb93054b90492fe6426
Ryan Cline <cline.ryan@gmail.com>
2017-02-17 13:21:53 -0700

    Updating dev dependencies to fix npm install errors. (#12)

--- src/tests/ApplyTheme-test.js
+++ src/tests/ApplyTheme-test.js
@@ -5,7 +5,7 @@
 
 import expect from 'expect';
 import React from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
+import TestUtils from 'react-addons-test-utils';
 import { ThemeContextTypes, getThemeContext } from '../ThemeContextTypes';
 import ApplyTheme from '../ApplyTheme';
 
